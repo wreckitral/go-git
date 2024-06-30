a11a740468c008b71504482cebb2a4f287bc4dc9
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-30 19:40:59 +0700

    feat: add hash-object command

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -46,6 +49,52 @@ func main() {
         parts := strings.Split(string(s), "\x00")
         fmt.Print(parts[1])
         r.Close()
+
+    case "hash-object":
+        if len(os.Args) < 3 {
+            fmt.Fprintf(os.Stderr, "usage: mygit hash-object -w [<args>...]\n")
+            os.Exit(1)
+        }
+
+        file, _ := os.ReadFile(os.Args[3])
+        stats, _ := os.Stat(os.Args[3])
+
+        content := string(file)
+        contentAndHeader := fmt.Sprintf("blob %d\x00%s", stats.Size(), content)
+
+        sha := sha1.Sum([]byte(contentAndHeader))
+        hash := fmt.Sprintf("%x", sha) // to base 16, with lower-case letters a-f
+
+        blobPath := fmt.Sprintf(".git/objects/%s/%s", hash[:2], hash[2:])
+
+        // write the bytes to this buffer variable
+        var buffer bytes.Buffer
+
+        z := zlib.NewWriter(&buffer)
+
+        _, err := z.Write([]byte(contentAndHeader))
+        if err != nil {
+            fmt.Printf("failed on file compression: %s", err.Error())
+        }
+        z.Close()
+
+        if err := os.MkdirAll(filepath.Dir(blobPath), os.ModePerm); err != nil {
+            fmt.Printf("failed on creating directory: %s", err.Error())
+        }
+
+        f, err := os.Create(blobPath)
+        if err != nil {
+            fmt.Printf("failed on creating file: %s", err.Error())
+        }
+        defer f.Close()
+
+        // write buffer to the file
+        _, err = f.Write(buffer.Bytes())
+        if err != nil {
+            fmt.Printf("failed on writing file: %s", err.Error())
+        }
+
+        fmt.Print(hash)
 
     default:
 	    fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
