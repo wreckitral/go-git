1dc5805ded999924449bc515f802d2696714aea6
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-07-01 08:35:26 +0700

    feat: add ls-tree command

--- cmd/main.go
+++ cmd/main.go
@@ -25,6 +25,9 @@ func main() {
     hashobjectFlag := flag.NewFlagSet("hash-object", flag.ExitOnError)
     writeObject := hashobjectFlag.Bool("w", false, "write the object into the object database")
 
+    lstreeFlag := flag.NewFlagSet("ls-tree", flag.ExitOnError)
+    nameOnly := lstreeFlag.Bool("name-only", false, "list only filenames")
+
 
 	switch command := os.Args[1]; command {
     case "init":
@@ -115,8 +118,96 @@ func main() {
 
         fmt.Print(hash)
 
+    case "ls-tree":
+        if err := lstreeFlag.Parse(os.Args[2:]); err != nil {
+            fmt.Fprintf(os.Stderr, "usage: mygit ls-tree <object>\n")
+            os.Exit(1)
+        }
+
+        if lstreeFlag.NArg() < 1 {
+            fmt.Fprintf(os.Stderr, "usage: mygit ls-tree <object>\n")
+            os.Exit(1)
+        }
+
+        sha := lstreeFlag.Arg(0)
+
+        if err := lsTree(sha, *nameOnly); err != nil {
+            fmt.Fprintf(os.Stderr, "ls-tree error: %v\n", err)
+            os.Exit(1)
+        }
+
     default:
 	    fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 	    os.Exit(1)
 	}
+}
+
+func lsTree(sha string, nameOnly bool) error {
+    dir, filename := sha[:2], sha[2:]
+
+    path := fmt.Sprintf(".git/objects/%s/%s", dir, filename)
+
+    file, err := os.Open(path)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    zlibReader, err := zlib.NewReader(file)
+    if err != nil {
+        return err
+    }
+    defer zlibReader.Close()
+
+    data, err := io.ReadAll(zlibReader)
+    if err != nil {
+        return err
+    }
+
+    if !bytes.HasPrefix(data, []byte("tree")) {
+        return fmt.Errorf("object is not a tree")
+    }
+
+    nullIndex := bytes.IndexByte(data, 0)
+    if nullIndex == -1 {
+        return fmt.Errorf("invalid tree object")
+    }
+
+    treeData := data[nullIndex+1:]
+    return parseTree(treeData, nameOnly)
+}
+
+func parseTree(data []byte, nameOnly bool) error {
+    for len(data) > 0 {
+        spaceIndex := bytes.IndexByte(data, ' ') // get the space character index
+        if spaceIndex == -1 {
+            return fmt.Errorf("invalid tree data")
+        }
+
+        mode := data[:spaceIndex] // get the content from before the space character index
+        data = data[spaceIndex+1:] // get the content from after the space character index
+
+        nullIndex := bytes.IndexByte(data, 0)
+        if nullIndex == -1 {
+            return fmt.Errorf("invalid tree data")
+        }
+
+        name := data[:nullIndex] // get data before the null index
+        data = data[nullIndex+1:] // get data after the null index
+
+        if len(data) < 20 {
+            return fmt.Errorf("invalid tree data")
+        }
+
+        sha := data[:20] // get the sha value
+        data = data[20:] // move data forward pass the sha value
+
+        if nameOnly {
+            fmt.Println(string(name))
+        } else {
+            fmt.Printf("%s %s %s\n", mode, fmt.Sprintf("%x", sha), name)
+        }
+    }
+
+    return nil
 }
