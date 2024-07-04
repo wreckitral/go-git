a764d673beac4480fc4d62b60b257bbfa32f2d83
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-07-04 18:40:34 +0700

    fix: fix write-tree command

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strings"
+	"path/filepath"
 )
 
 func main() {
@@ -55,15 +55,37 @@ func main() {
 
         sha := catfileFlag.Arg(0)
 
-        path := fmt.Sprintf(".git/objects/%s/%s", sha[:2], sha[2:])
+        path := filepath.Join(".git", "objects", sha[:2], sha[2:])
 
-        file, _ := os.Open(path)
-        r, _ := zlib.NewReader(io.Reader(file))
-        s, _ := io.ReadAll(r)
+        b, err := os.ReadFile(path)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "error on cat-file: %s", err.Error())
+            os.Exit(1)
+        }
 
-        parts := strings.Split(string(s), "\x00")
-        fmt.Print(parts[1])
-        r.Close()
+        buf := bytes.NewBuffer(b)
+
+        r, err := zlib.NewReader(buf)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "error on cat-file: %s", err.Error())
+            os.Exit(1)
+        }
+        defer r.Close()
+
+        names, err := io.ReadAll(r)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "error on cat-file: %s", err.Error())
+            os.Exit(1)
+        }
+
+        found := false
+        for _, name := range names {
+            if found {
+                fmt.Print(string(name))
+            } else {
+                found = name == 0
+            }
+        }
 
     case "hash-object":
         if err := hashobjectFlag.Parse(os.Args[2:]); err != nil {
@@ -106,102 +128,19 @@ func main() {
         }
 
     case "write-tree":
-        hash, err := writeTree(".")
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error writing tree:", err)
-			os.Exit(1)
-		}
-		fmt.Println(hash)
+        currentDir, _ := os.Getwd()
 
+        treeHash, c := getTreeHash(currentDir)
+
+        if err := writeObject(treeHash, c); err != nil {
+            fmt.Fprintf(os.Stderr, "ls-tree error: %v\n", err)
+            os.Exit(1)
+        }
+
+        fmt.Println(treeHash)
     default:
 	    fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 	    os.Exit(1)
 	}
 }
 
-func lsTree(sha string, nameOnly bool) error {
-    dir, filename := sha[:2], sha[2:]
-
-    path := fmt.Sprintf(".git/objects/%s/%s", dir, filename)
-
-    file, err := os.Open(path)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-
-    zlibReader, err := zlib.NewReader(file)
-    if err != nil {
-        return err
-    }
-    defer zlibReader.Close()
-
-    data, err := io.ReadAll(zlibReader)
-    if err != nil {
-        return err
-    }
-
-    if !bytes.HasPrefix(data, []byte("tree")) {
-        return fmt.Errorf("object is not a tree")
-    }
-
-    nullIndex := bytes.IndexByte(data, 0)
-    if nullIndex == -1 {
-        return fmt.Errorf("invalid tree object")
-    }
-
-    treeData := data[nullIndex+1:]
-    return parseTree(treeData, nameOnly)
-}
-
-func parseTree(data []byte, nameOnly bool) error {
-    for len(data) > 0 {
-        spaceIndex := bytes.IndexByte(data, ' ') // get the space character index
-        if spaceIndex == -1 {
-            return fmt.Errorf("invalid tree data")
-        }
-
-        mode := data[:spaceIndex] // get the content from before the space character index
-        data = data[spaceIndex+1:] // get the content from after the space character index
-
-        nullIndex := bytes.IndexByte(data, 0)
-        if nullIndex == -1 {
-            return fmt.Errorf("invalid tree data")
-        }
-
-        name := data[:nullIndex] // get data before the null index
-        data = data[nullIndex+1:] // get data after the null index
-
-        if len(data) < 20 {
-            return fmt.Errorf("invalid tree data")
-        }
-
-        sha := data[:20] // get the sha value
-        data = data[20:] // move data forward pass the sha value
-
-        if nameOnly {
-            fmt.Println(string(name))
-        } else {
-            fmt.Printf("%s %s %s\n", mode, fmt.Sprintf("%x", sha), name)
-        }
-    }
-
-    return nil
-}
-
-func writeTree(dir string) (string, error) {
-	entries, err := collectEntries(dir)
-	if err != nil {
-		return "", err
-	}
-
-	treeContent := formatTree(entries)
-	treeHash := hashObject("tree", treeContent)
-
-	err = writeObject(treeHash, treeContent)
-	if err != nil {
-		return "", err
-	}
-
-	return treeHash, nil
-}
--- cmd/util.go
+++ cmd/util.go
@@ -6,60 +6,80 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"sort"
 )
 
-type Entry struct {
-	mode string
-	name string
-	hash []byte
+func lsTree(sha string, nameOnly bool) error {
+    dir, filename := sha[:2], sha[2:]
+
+    path := fmt.Sprintf(".git/objects/%s/%s", dir, filename)
+
+    file, err := os.Open(path)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    zlibReader, err := zlib.NewReader(file)
+    if err != nil {
+        return err
+    }
+    defer zlibReader.Close()
+
+    data, err := io.ReadAll(zlibReader)
+    if err != nil {
+        return err
+    }
+
+    if !bytes.HasPrefix(data, []byte("tree")) {
+        return fmt.Errorf("object is not a tree")
+    }
+
+    nullIndex := bytes.IndexByte(data, 0)
+    if nullIndex == -1 {
+        return fmt.Errorf("invalid tree object")
+    }
+
+    treeData := data[nullIndex+1:]
+    return parseTree(treeData, nameOnly)
 }
 
-func collectEntries(dir string) ([]Entry, error) {
-	var entries []Entry
-	files, err := os.ReadDir(dir)
-	if err != nil {
-		return nil, err
-	}
+func parseTree(data []byte, nameOnly bool) error {
+    for len(data) > 0 {
+        spaceIndex := bytes.IndexByte(data, ' ') // get the space character index
+        if spaceIndex == -1 {
+            return fmt.Errorf("invalid tree data")
+        }
 
-	for _, file := range files {
-		if file.Name() == ".git" {
-			continue
-		}
+        mode := data[:spaceIndex] // get the content from before the space character index
+        data = data[spaceIndex+1:] // get the content from after the space character index
 
-		mode := "100644"
-		if file.IsDir() {
-			mode = "40000"
-			subEntries, err := writeTree(filepath.Join(dir, file.Name()))
-			if err != nil {
-				return nil, err
-			}
-			sha, _ := hex.DecodeString(subEntries)
-			entries = append(entries, Entry{mode, file.Name(), sha})
-		} else {
-			content, err := os.ReadFile(filepath.Join(dir, file.Name()))
-			if err != nil {
-				return nil, err
-			}
-			blobHash := hashObject("blob", content)
-			sha, _ := hex.DecodeString(blobHash)
-			entries = append(entries, Entry{mode, file.Name(), sha})
-		}
-	}
+        nullIndex := bytes.IndexByte(data, 0)
+        if nullIndex == -1 {
+            return fmt.Errorf("invalid tree data")
+        }
 
-    sort.Slice(entries, func(i, j int) bool { return entries[i].name < entries[j].name })
-	return entries, nil
-}
+        name := data[:nullIndex] // get data before the null index
+        data = data[nullIndex+1:] // get data after the null index
 
-func formatTree(entries []Entry) []byte {
-	var buf bytes.Buffer
-	for _, entry := range entries {
-		fmt.Fprintf(&buf, "%s %s\x00", entry.mode, entry.name)
-		buf.Write(entry.hash)
-	}
-	return buf.Bytes()
+        if len(data) < 20 {
+            return fmt.Errorf("invalid tree data")
+        }
+
+        sha := data[:20] // get the sha value
+        data = data[20:] // move data forward pass the sha value
+
+        if nameOnly {
+            fmt.Println(string(name))
+        } else {
+            fmt.Printf("%s %s %s\n", mode, fmt.Sprintf("%x", sha), name)
+        }
+    }
+
+    return nil
 }
 
 func hashObject(objectType string, content []byte) string {
@@ -75,16 +95,70 @@ func writeObject(hash string, content []byte) error {
 	dir := filepath.Join(".git", "objects", hash[:2])
 	file := filepath.Join(dir, hash[2:])
 
-	if err := os.MkdirAll(dir, 0755); err != nil {
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
 		return err
 	}
 
 	var buf bytes.Buffer
-	zlibWriter := zlib.NewWriter(&buf)
-	defer zlibWriter.Close()
-	if _, err := zlibWriter.Write(content); err != nil {
+	w := zlib.NewWriter(&buf)
+	if _, err := w.Write(content); err != nil {
 		return err
 	}
+    w.Close()
 
 	return os.WriteFile(file, buf.Bytes(), 0644)
+}
+
+func getTreeHash(dir string) (string, []byte) {
+    files, _ := os.ReadDir(dir)
+
+    type entry struct {
+        name string
+        con []byte
+    }
+
+    var entries []entry
+    conSize := 0
+
+    for _, file := range files {
+        if file.Name() == ".git" {
+            continue
+        }
+
+        if file.IsDir() {
+            b, _ := getTreeHash(filepath.Join(dir, file.Name()))
+            s := fmt.Sprintf("40000 %s\x00", file.Name())
+            byts := append([]byte(s), b...)
+            entries = append(entries, entry{file.Name(), byts})
+            conSize += len(byts)
+        } else {
+            f, _ := os.Open(filepath.Join(dir, file.Name()))
+            b, _ := io.ReadAll(f)
+            s := fmt.Sprintf("blob %d\x00%s", len(b), string(b))
+
+            sha1 := sha1.New()
+            _, err := io.WriteString(sha1, s)
+            if err != nil {
+                return "", nil
+            }
+
+			s = fmt.Sprintf("100644 %s\u0000", file.Name())
+			b = append([]byte(s), sha1.Sum(nil)...)
+			entries = append(entries, entry{file.Name(), b})
+			conSize += len(b)
+        }
+    }
+    sort.Slice(entries, func(i, j int) bool { return entries[i].name < entries[j].name })
+
+    s := fmt.Sprintf("tree %d\u0000", conSize)
+    b := []byte(s)
+    for _, entry := range entries {
+        b = append(b, entry.con...)
+    }
+    sha1 := sha1.New()
+    _, err := io.WriteString(sha1, string(b))
+    if err != nil {
+        return "", nil
+    }
+    return hex.EncodeToString(sha1.Sum(nil)), b
 }
